158f75b4756eb84047b791007639b8bd0a8a6f7c
Rishi Pardeshi <93481628+RishiPardeshi@users.noreply.github.com>
2021-11-05 12:56:56 +0530

    Get In Touch
    Added get in touch form to interact with users

--- logic.js
+++ logic.js
@@ -116,3 +116,144 @@ document.addEventListener( 'DOMContentLoaded', function () {
 		  rewind: true
 	} ).mount();
   } );
+
+let Name = document.getElementById('name');
+let email = document.getElementById('email');
+let message = document.getElementById('message');
+let nameError = document.getElementById('nameError');
+let emailError = document.getElementById('emailError');
+let messageError = document.getElementById('messageError');
+let nameCheck = document.getElementById('nameCheck');
+let emailCheck = document.getElementById('emailCheck');
+let messageCheck = document.getElementById('messageCheck');
+let btn = document.getElementById('btn');
+
+let nameC = false
+let mailC = false
+let msgC = false
+
+let status = document.getElementById('status');
+let why = document.getElementById('why');
+let submitted = document.getElementById('submitted');
+
+// let form = document.forms.Myform;
+let form = document.getElementById('Myform');
+
+Name.addEventListener('input',()=>{
+	if(Name.value.length < 3 || Name.value.length > 16){
+		Name.style.border = '1px solid #ff195e';
+		nameError.style.visibility = 'visible';
+		nameError.innerHTML = 'name should be greater than 3 or less than 16 characters'
+		nameCheck.classList.add('far','fa-times-circle')
+		nameCheck.style.color = '#ff195e'
+		nameCheck.style.visibility = 'visible'
+		nameC = false
+	}
+	else{
+		Name.style.border = '1px solid #72c0ff';
+		nameCheck.classList.remove('far','fa-times-circle')
+		nameCheck.classList.add('far','fa-check-circle')
+		nameCheck.style.color = '#00e91f'
+		nameCheck.style.visibility = 'visible'
+		nameError.style.visibility = 'hidden';
+		nameC = true
+	}
+	validateCheck()
+})
+function validateEmail(email) {
+    const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(email).toLowerCase());
+}
+email.addEventListener('input',()=>{
+	check = validateEmail(email.value)
+	if(check == false){
+		email.style.border = '1px solid #ff195e';
+		emailError.style.visibility = 'visible';
+		emailError.innerHTML = 'invalid email'
+		emailCheck.classList.add('far','fa-times-circle')
+		emailCheck.style.color = '#ff195e'
+		emailCheck.style.visibility = 'visible'
+		mailC = false
+	}
+	else{
+		email.style.border = '1px solid #72c0ff';
+		emailCheck.classList.remove('far','fa-times-circle')
+		emailCheck.classList.add('far','fa-check-circle')
+		emailCheck.style.color = '#00e91f'
+		emailCheck.style.visibility = 'visible'
+		emailError.style.visibility = 'hidden';
+		mailC = true
+	}
+	validateCheck()
+})
+message.addEventListener('input',()=>{
+	if(message.value.length < 12 || message.value.length > 100){
+		message.style.border = '1px solid #ff195e';
+		messageError.style.visibility = 'visible';
+		messageError.innerHTML = 'message should be greater than 12 or less than 100 characters';
+		messageCheck.classList.add('far','fa-times-circle')
+		messageCheck.style.color = '#ff195e'
+		messageCheck.style.visibility = 'visible'
+		msgC = false
+	}
+	else{
+		message.style.border = '1px solid #72c0ff';
+		messageCheck.classList.remove('far','fa-times-circle')
+		messageCheck.classList.add('far','fa-check-circle')
+		messageCheck.style.color = '#00e91f'
+		messageCheck.style.visibility = 'visible'
+		messageError.style.visibility = 'hidden';
+		msgC = true
+	}
+	validateCheck()
+})
+
+
+async function handleSubmit(event) {
+	event.preventDefault();
+	var data = new FormData(event.target);
+	fetch(event.target.action, {
+	  method: form.method,
+	  body: data,
+	  headers: {
+		  'Accept': 'application/json'
+	  }
+	}).then(response => {
+		// submitted.classList.remove('fas','fa-times-circle')
+		submitted.classList.add('fas','fa-check-circle')
+		why.innerHTML = 'form submitted successfully';
+		status.style.background = '#01b719';
+		status.style.left = '0px';
+		nameCheck.style.visibility = 'hidden';
+		emailCheck.style.visibility = 'hidden';
+		messageCheck.style.visibility = 'hidden';
+		setTimeout(() => {
+			status.style.left = '-450px';
+		}, 3400);
+	  form.reset();
+	}).catch(error => {
+		why.innerHTML = 'Opps! something went wrong';
+		status.style.background = '#ff0858';
+		status.style.left = '0px';
+		submitted.classList.add('fas','fa-times-circle')
+		// submitted.classList.remove('fas','fa-check-circle')
+		nameCheck.style.visibility = 'hidden';
+		emailCheck.style.visibility = 'hidden';
+		messageCheck.style.visibility = 'hidden';
+		setTimeout(() => {
+			status.style.left = '-450px';
+		}, 3400);
+	})
+  }
+
+function validateCheck() {
+	if (nameC == true && mailC == true && msgC == true){
+		btn.disabled = false
+		btn.style.cursor = 'pointer'
+	}
+	else{
+		btn.style.cursor = 'not-allowed'
+		btn.disabled = true
+	}
+}
+form.addEventListener('submit',handleSubmit)
